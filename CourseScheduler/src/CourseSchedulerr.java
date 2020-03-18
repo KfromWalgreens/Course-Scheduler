68bb96a79bc87c34e07c46e07f7dfc458562b765
Kennedy Marsh <kikimarsholl@gmail.com>
2020-03-18 15:23:17 -0400

    Completed part 4
    part 4

--- CourseScheduler/src/CourseSchedulerr.java
+++ CourseScheduler/src/CourseSchedulerr.java
@@ -1,3 +1,7 @@
+
+import java.util.*;
+import java.io.*;
+
 public class CourseSchedulerr {
     public static void main(String[] args){
         //problem 1
@@ -18,5 +22,38 @@ public class CourseSchedulerr {
         System.out.println(course1.toString());
         System.out.println(course2.toString());
         System.out.println(course3.toString());
+
+        //problem 4
+        ArrayList<Course> courses = new ArrayList<Course>();
+        String in = "coursedata.txt";
+        try {
+            readCourseData(courses, in);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void readCourseData(ArrayList courses, String filename) throws FileNotFoundException {
+        Scanner input = new Scanner(new File(filename));
+        while(input.hasNextLine()) {
+            String wat = input.nextLine();
+            String[] info = wat.split(",");
+            String name = info[0];
+            String semester = info[1];
+            String the = input.nextLine();
+            String[] info2 = the.split("\\* ");
+            String fname =  info2[0];
+            String lname =  info2[1];
+            String building =  info2[2];
+            int num =  Integer.parseInt(info2[3]);
+            Instructor ins = new Instructor(fname, lname, building, num);
+            String title =  input.nextLine();
+            String pub =  input.nextLine();
+            int ed =  Integer.parseInt(input.nextLine());
+            Textbook book = new Textbook(title, pub, ed);
+            Course course = new Course(name, semester, ins, book);
+            courses.add(course);
+            System.out.println(course.toString());
+        }
     }
 }
